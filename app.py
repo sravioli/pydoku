4d79911af16af0fa23535515365f46fc78be7ad5
Simon <fsimone2002@gmail.com>
2022-07-22 18:26:32 +0200

    fix: update code to match changes in modules

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
+from loguru import logger
 from source import ImageProcessor, SudokuExtractor, SudokuSolver
 from keras.models import load_model
+import numpy as np
+
 import cv2
+
+logger.remove()
 
 
 def main():
@@ -12,10 +17,10 @@ def main():
     slv = SudokuSolver.SudokuSolver()
 
     # load preferred model
-    model = load_model("./source/LeNet5+")
+    model = load_model("./source/LeNet5")
 
     # import image
-    original_image = ipr.read("./source/test_imgs/sudoku.jpg")
+    original_image = ipr.read("./source/test_imgs/1.jpg")
 
     # preprocess image
     image = ipr.preprocess_image(original_image)
@@ -29,17 +34,22 @@ def main():
     # warp image (this also crops it)
     warped_image = sxt.warp_image(image, grid_corners)
 
+    # remove grid from image
+    image = sxt.remove_grid(warped_image)
+
     # depending on the image, a rotation may be necessary
-    # image = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    image = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
     # extract every sudoku cell from the warped image
-    cells = sxt.extract_cells(warped_image)
+    cells = sxt.extract_cells(image)
 
     # construct grid of numbers from the list of cells
-    board = sxt.construct_board(model, cells)
+    board = sxt.construct_board(model, cells, 1200)
 
     # can print unsolved grid
     slv.print(board)
+
+    sxt.error_check(board)
 
     slv.solve(board)
     slv.print(board)
